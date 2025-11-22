011fcf6e2d5ad0de5f381241f9196e047e5f82fe
basiliok <paledot01@gmail.com>
2025-11-22 18:46:29 -0500

    update login design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,43 +1,68 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { GitHubIcon } from '../assets/icons/GitHubIcon';
 
 export const Login = () => {
-    const [email, setEmail] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const navigate = useNavigate();
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        if (email && password) navigate('/home');
+        if (username && password) navigate('/home');
     };
 
     return (
-        <div className="flex min-h-screen items-center justify-center">
-            <form onSubmit={handleSubmit} className="w-80 space-y-4 rounded bg-[#a7a7a72c] p-6 shadow">
-                <h2 className="text-center text-xl font-bold">Login</h2>
+        <div className="text-primary-text flex h-full flex-col justify-between">
+            <div className="mt-25 flex flex-col items-center gap-6">
+                <div className="flex flex-col items-center gap-1">
+                    <img src="/peluchin.png" alt="Logo" className="h-16 w-16" />
+                    <h1 className="text-size-lg leading-none font-semibold">Sign in</h1>
+                </div>
+                <form onSubmit={handleSubmit} className="flex w-80 flex-col gap-4">
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="username" className="text-size-sm font-semibold">
+                            Username
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            className="border-input-border w-full rounded-md border px-4 py-2"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
 
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className="w-full rounded border px-3 py-2"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="w-full rounded border px-3 py-2"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-
-                <button type="submit" className="w-full rounded bg-blue-500 py-2 text-white hover:bg-blue-600">
-                    Login
-                </button>
-            </form>
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="password" className="text-size-sm font-semibold">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="border-input-border w-full rounded-md border px-4 py-2"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-button-bg hover:bg-button-hover-bg text-size-sm w-full cursor-pointer rounded-md py-2 font-semibold"
+                    >
+                        Sign in
+                    </button>
+                </form>
+            </div>
+            <footer className="text-secondary-text text-size-sm flex justify-center pb-4">
+                <div className="hover:text-primary-text flex cursor-pointer flex-row items-center justify-center gap-1 transition-colors">
+                    <GitHubIcon size={24} />
+                    <a href="https://github.com/basiliok" target="_blank" rel="noopener noreferrer">
+                        basiliok
+                    </a>
+                </div>
+            </footer>
         </div>
     );
 };
